519ba09311e15c312838bde2d5029c5145ba6b43
Prodyut Samanta <samantaprodyut@gmail.com>
2026-08-19 23:06:07 +0530

    Inital Commit

---
+++ src/test/java/com/auro/festival/festival/FestivalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auro.festival.festival;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestivalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
